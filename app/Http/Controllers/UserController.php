379ae8c9774d79293dddb2caf8b132ef5645ef1a
Estudiante <estuam@UAM.LOCAL>
2017-03-11 09:22:55 -0500

    fix app user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use App\user;
+use App\User;
 class UserController extends Controller
 {
     //
